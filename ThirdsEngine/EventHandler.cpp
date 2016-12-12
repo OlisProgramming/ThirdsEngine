8e36b9d68e4e743602392f08fc132de46cab9952
ScrollFusionAdmin <scrollfusion@gmail.com>
2016-12-12 19:35:09 +0000

    Input from keyboard beginning

--- ThirdsEngine/EventHandler.cpp
+++ ThirdsEngine/EventHandler.cpp
@@ -1,10 +1,20 @@
 #include "EventHandler.h"
 
+#include <SDL.h>
+
 #include "GameManager.h"
 
 namespace te {
 
+	EventHandler::EventHandler() {
+		keyState = SDL_GetKeyboardState(NULL);
+	}
+
 	void EventHandler::close() {
 		GameManager::manager->requestClose();
+	}
+
+	bool EventHandler::getKey(Key keycode) {
+		return keyState[keycode];
 	}
 }
