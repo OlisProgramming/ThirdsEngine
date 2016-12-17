671aeb0c99f7892cd2c97d6a0f42a07dcbd7cb4c
ScrollFusionAdmin <scrollfusion@gmail.com>
2016-12-17 12:34:36 +0000

    GLM macros for abstraction

--- ThirdsEngine/Math.h
+++ ThirdsEngine/Math.h
@@ -1,6 +1,15 @@
 #pragma once
 
-#include <glm\glm.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#define Mat4Translate(x, y, z)	glm::translate(x, y, z)
+#define Mat4Scale(x, y, z)		glm::scale(x, y, z)
+#define Mat4ScaleUniform(s)		glm::scale(s, s, s)
+#define Mat4Rotate(theta, axis)	glm::rotate(theta, axis)
+
+#define Mat4Transform(translate, rotate, scale)		((translate) * (rotate) * (scale))
 
 namespace te {
 	typedef glm::vec2 Vec2;
--- ThirdsEngine/Shader.h
+++ ThirdsEngine/Shader.h
@@ -23,7 +23,7 @@ namespace te {
 				VertexShaderStream.close();
 			}
 			else {
-				printf("Impossible to open %s. Are you in the right directory ? Don't forget to read the FAQ !\n", vertex_file_path);
+				printf("Could not open shader %s.", vertex_file_path);
 				getchar();
 				return 0;
 			}
@@ -37,13 +37,18 @@ namespace te {
 					FragmentShaderCode += "\n" + Line;
 				FragmentShaderStream.close();
 			}
+			else {
+				printf("Could not open shader %s.", fragment_file_path);
+				getchar();
+				return 0;
+			}
 
 			GLint Result = GL_FALSE;
 			int InfoLogLength;
 
 
 			// Compile Vertex Shader
-			printf("Compiling shader : %s\n", vertex_file_path);
+			printf("Compiling shader %s...\n", vertex_file_path);
 			char const * VertexSourcePointer = VertexShaderCode.c_str();
 			glShaderSource(VertexShaderID, 1, &VertexSourcePointer, NULL);
 			glCompileShader(VertexShaderID);
@@ -54,13 +59,13 @@ namespace te {
 			if (InfoLogLength > 0) {
 				std::vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
 				glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
-				printf("%s\n", &VertexShaderErrorMessage[0]);
+				printf("%s", &VertexShaderErrorMessage[0]);
 			}
 
 
 
 			// Compile Fragment Shader
-			printf("Compiling shader : %s\n", fragment_file_path);
+			printf("Compiling shader %s...\n", fragment_file_path);
 			char const * FragmentSourcePointer = FragmentShaderCode.c_str();
 			glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer, NULL);
 			glCompileShader(FragmentShaderID);
@@ -71,7 +76,7 @@ namespace te {
 			if (InfoLogLength > 0) {
 				std::vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
 				glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
-				printf("%s\n", &FragmentShaderErrorMessage[0]);
+				printf("%s", &FragmentShaderErrorMessage[0]);
 			}
 
 
